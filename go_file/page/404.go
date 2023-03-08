e68d7ee02aef3b470c5d001d932004af9bd41c62
fleo <leo.faure@ynov.com>
2023-03-08 14:01:05 +0100

    refractor 404 page

--- go_file/page/404.go
+++ go_file/page/404.go
@@ -6,14 +6,13 @@ import (
 	"net/http"
 )
 
-func Error(w http.ResponseWriter, r *http.Request) {
+func Error(w http.ResponseWriter, _ *http.Request) {
 	page, _ := template.ParseFiles("./source/templates/404.html")
 	bdd.Nbr = RandomInt()
 	err := page.ExecuteTemplate(w, "404.html", bdd)
 	if err != nil {
 		return
 	}
-	//TODO : 404 different
 }
 
 func RandomInt() int {
