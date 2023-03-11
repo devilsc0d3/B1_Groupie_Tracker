c8595d38e85f246fd02fbc4262e286029ae7b017
fleo <leo.faure@ynov.com>
2023-03-11 13:52:24 +0100

    add ester egg todo

--- go_file/page/404.go
+++ go_file/page/404.go
@@ -7,6 +7,7 @@ import (
 )
 
 func Error(w http.ResponseWriter, _ *http.Request) {
+	//TODO : ester eggs
 	page, _ := template.ParseFiles("./source/templates/404.html")
 	bdd.Nbr = RandomInt()
 	err := page.ExecuteTemplate(w, "404.html", bdd)
